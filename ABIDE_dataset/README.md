fed19d8d0f04482e36b1909ac6c4696fe88802ad
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-05 21:54:48 +0100

    #4 ABIDE dataset loading

---
+++ ABIDE_dataset/README.md
@@ -0,0 +1,50 @@
+# Application to ABIDE Dataset
+
+ABIDE  database [[2]](#2) is one of the dataset used by the author of the studied article. It is freely provided py the [Preprocessed Connectome Project](http://preprocessed-connectomes-project.org/abide/) initiative and can be downloaded and prepocessed using the Python package [*nilearn*](https://nilearn.github.io/dev/introduction.html#what-is-nilearn) which allows users to apply machine learning techniques to analyse data acquired with MRI machines. 
+
+
+## What are the data ?
+Even if preprocessed fMRI images are available, those files are very large, i.e. ~100 Mo each. However, the extracted time series files are ~200Ko in size for each subject. We downloaded *rois-ho.1D* files which are time series extracted from Harvard-Oxford (HO) atlas. 
+
+In medical imaging, time series are a collection of data points that are recorded or measured over a period of time. In the cas of fMRI, time series represent the intensity of brain activity within various regions at different time points as the MRI scanner captures images over time. The HO atlas is a set of anatomical brain regions or regions of interest (ROIs) that have been defined and labeled based on the human brain's anatomical structures.
+
+Here, the time series data is derived from the specific regions defined in the HO atlas. In our code, they are numpy array of size (timepoints x regions), here it is (196 x 111).
+
+
+Like the authors [[1]](#1), we use *nilearn* to preprocess the data by specifying necessary parameters :
+- *pipeline* : Configurable Pipeline for the Analysis of Connectomes (C-PAC) <sup id="s1">[1](#f1)</sup>. 
+- *strategy* : filt_noglobal which means band-pass filtering only without global signal regression
+
+## How to download those data ?
+
+The following tuto enables you to download preprocessed extracted time series and the connectivity matrix associated with each patient. Note that, in this context, the connectivity matrix is a matrix of a connectivity measure (e. g. Pearson's correlation) between two time series of the brain activity of a subject. Its size is, thereby, (number of regions x number of regions).
+
+1. activate the virtual environment .venv
+2. copy paste the following commant in the terminal :
+```
+python download_preprocess.py -o "path/to/data"
+```
+If you want to load less data (e.g. 400) than the default value (871), you can type in :
+```
+python download_preprocess.py -o "path/to/data" -n 400
+
+```
+
+## How are the feature vectors computed ?
+*Recursive feature elimination* (RFE) with scikit learn: Given an external estimator (here, a Ridge Classifier) that assigns weights to features, the goal of RFE is to select features by recursively considering smaller and smaller sets of features. First, the estimator is trained on the initial set of features and the importance of each feature is obtained through any specific attribute. Then, the least important features are pruned from current set of features. That procedure is recursively repeated until the desired number of features to select is eventually reached.
+
+**Fisher transformed Pearson's correlation coefficient**
+
+## What new hypotheses are we going to test ?
+
+- a new representation of feature vector ?
+- another way to classify the subject ?
+
+
+---
+<a id="f1">1</a> A connectome is a functional-connectivity matrix between a set of brain regions of interest (ROIs). [↩](#s1)
+
+## Reference 
+<a id="1"> [1] </a> S. Parisot et al., ‘Spectral Graph Convolutions for Population-based Disease Prediction’. arXiv, Jun. 21, 2017. doi: 10.48550/arXiv.1703.03020.
+
+<a id="data"> [2] </a> Cameron Craddock, Yassine Benhajali, Carlton Chu, Francois Chouinard, Alan Evans, András Jakab, Budhachandra Singh Khundrakpam, John David Lewis, Qingyang Li, Michael Milham, Chaogan Yan, Pierre Bellec (2013). The Neuro Bureau Preprocessing Initiative: open sharing of preprocessed neuroimaging data and derivatives. In Neuroinformatics 2013, Stockholm, Sweden.
---
+++ ABIDE_dataset/download_preprocess.py
@@ -0,0 +1,109 @@
+# Copyright (C) 2017 Sarah Parisot <s.parisot@imperial.ac.uk>, Sofia Ira Ktena <ira.ktena@imperial.ac.uk>
+# This code is written from the github : https://github.com/parisots/population-gcn/tree/master of the studied article
+
+import argparse
+from nilearn import datasets
+import os
+import shutil
+import numpy as np
+import ABIDE_graph as Reader
+
+
+# Get the list of subject IDs
+def get_ids(data_folder, num_subjects=None):
+    """
+
+    return:
+        subject_IDs    : list of all subject IDs
+    """
+
+    subject_IDs = np.genfromtxt(os.path.join(data_folder, 'subject_IDs.txt'), dtype=str)
+
+    if num_subjects is not None:
+        subject_IDs = subject_IDs[:num_subjects]
+
+    return subject_IDs
+
+def fetch_filenames(data_folder, subject_IDs, file_type):
+
+    """
+        subject_list : list of short subject IDs in string format
+        file_type    : must be one of the available file types
+
+    returns:
+
+        filenames    : list of filetypes (same length as subject_list)
+    """
+
+    import glob
+
+    # Specify file mappings for the possible file types
+    filemapping = {'func_preproc': '_func_preproc.nii.gz',
+                   'rois_ho': '_rois_ho.1D'}
+
+    # The list to be filled
+    filenames = []
+
+    # Fill list with requested file paths
+    for i in range(len(subject_IDs)):
+        os.chdir(data_folder)  # change the current/working directory to the directory where the data are stored
+        try:
+            filenames.append(glob.glob('*' + subject_IDs[i] + filemapping[file_type])[0])
+        except IndexError:
+            # Return N/A if subject ID is not found
+            filenames.append('N/A')
+
+    return filenames
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='Download preprocessed datain chosen directory')
+    parser.add_argument('-o', '--out_dir', required=True, type=str, help='Path to local folder to download files to')
+    parser.add_argument('-n', '--nb_subjects', type=int, default=871, help='Number of data/subject to download')
+    args = parser.parse_args()
+
+    # Selected pipeline
+    pipeline = 'cpac'
+
+    # Input data variables
+    num_subjects = args.nb_subjects # Number of subjects
+    root_folder =  args.out_dir #'/path/to/data/'
+    data_folder = os.path.join(root_folder, 'ABIDE_pcp/cpac/filt_noglobal')
+
+    # Files to fetch
+    files = ['rois_ho']
+
+    filemapping = {'func_preproc': 'func_preproc.nii.gz', # to you use for fMRI images
+                'rois_ho': 'rois_ho.1D'}
+
+    if not os.path.exists(data_folder): os.makedirs(data_folder)
+    shutil.copyfile('./subject_IDs.txt', os.path.join(data_folder, 'subject_IDs.txt'))
+
+    # Download database files
+    abide = datasets.fetch_abide_pcp(data_dir=root_folder, n_subjects=num_subjects, pipeline=pipeline,
+                                    band_pass_filtering=True, global_signal_regression=False, derivatives=files)
+
+
+    subject_IDs = get_ids(data_folder=data_folder, num_subjects=num_subjects)
+    subject_IDs = subject_IDs.tolist()
+
+    # Create a folder for each subject
+    for s, fname in zip(subject_IDs, fetch_filenames(data_folder=data_folder, subject_IDs=subject_IDs, file_type=files[0])):
+        subject_folder = os.path.join(data_folder, s)
+        if not os.path.exists(subject_folder):
+            os.mkdir(subject_folder)
+
+        # Get the base filename for each subject
+        base = fname.split(files[0])[0]
+
+        # Move each subject file to the subject folder
+        for fl in files:
+            if not os.path.exists(os.path.join(subject_folder, base + filemapping[fl])):
+                shutil.move(base + filemapping[fl], subject_folder)
+
+    # Compute and save connectivity matrices
+    time_series = Reader.get_timeseries(data_folder=data_folder, subject_list=subject_IDs, atlas_name='ho')
+    for i in range(len(subject_IDs)):
+            Reader.compute_subject_connectivity(time_series[i], subject_IDs[i], 'ho', 'correlation', save_path=data_folder)
+
