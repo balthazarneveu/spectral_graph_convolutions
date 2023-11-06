b2589f36ead3596d90c0e4c6864e81845acc45ac
InesVATI <ines.vati@eleves.enpc.fr>
2023-11-07 00:36:47 +0100

    #5 application

--- notes-on-graph-spectral-theory/graph_convolution_example.py
+++ notes-on-graph-spectral-theory/graph_convolution_example.py
@@ -1,16 +1,95 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from random import choice, random
+
+import torch
+from torch_geometric.nn import ChebConv
 
 def create_simple_graph(nbNodes=5):
     G = nx.Graph()
-    G.add_nodes_from(range(nbNodes))
-    for i in range(1, nbNodes):
-        G.add_edge(0, i)
+    #G.add_nodes_from(range(nbNodes))
+    #random_position = list(range(nbNodes))
+
+    """for i in range(nbNodes):
+        rd_weight = random()
+        rd_node = choice(random_position)
+        if i == rd_node:
+            rd_node = choice(random_position)
+        G.add_node(i, feat=[i])
+        G.add_edge(rd_node, i, weight=rd_weight) """
+    
+    G.add_edge(1, 2, weight=0.6)
+    G.add_edge(1, 3, weight=0.2)
+    G.add_edge(3, 4, weight=0.1)
+    G.add_edge(3, 5, weight=0.7)
+    G.add_edge(3, 6, weight=0.9)
+    G.add_edge(1, 4, weight=0.3)
+
+    # add node features
+    G.nodes[1]["feat"] = [1]
+    G.nodes[2]["feat"] = [2]
+    G.nodes[3]["feat"] = [3]
+    G.nodes[4]["feat"] = [4]
+    G.nodes[5]["feat"] = [5]
+    G.nodes[6]["feat"] = [6]
 
     return G
 
+def draw_weighted_graph(G):
+
+    # ---Visualization 
+    pos = nx.spring_layout(G, seed=7) # positions for all nodes - seed for reproducibility
+    nx.draw_networkx_nodes(G, pos, node_size=700) # nodes
+    nx.draw_networkx_edges(G, pos, width=6) # edges
+
+    # node feature
+    node_features = nx.get_node_attributes(G, "feat")
+    nx.draw_networkx_labels(G, pos, labels=node_features) #font_size=20, font_family="sans-serif") # node labels
+    
+    # edge weight labels
+    edge_weights = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos, edge_weights)
+
+    ax = plt.gca() # to get the current axis
+    ax.margins(0.08) # to avoid the nodes being clipped
+    plt.axis("off") # to turn of the axis
+    plt.tight_layout() # to make sure nothing gets clipped
+    plt.show()
+
+def compute_graph_convolution(graph, K=1, out_channels=2, normalization='sym'):
+    # get graph dim
+    edge_weights = torch.tensor([G[u][v]['weight'] for u, v in G.edges()]) # (|E|, )
+    print('weights', edge_weights.size())
+    node_features = torch.tensor([G.nodes[i]['feat'] for i in G.nodes()]) # (|V|, feat_dim)
+    print('node_features', node_features.size())
+    edge_indices = torch.tensor([e for e in G.edges()]).permute(1, 0) # (2, |E|) 
+    print('edge_indices', edge_indices.size())
+
+    # compute graph convolution
+    conv = ChebConv(
+        in_channels=node_features.shape[1],
+        out_channels=out_channels, K=K,
+        normalization=normalization
+        )
+
+    out = conv(node_features, edge_indices, edge_weights) # (|V|, out_channels)
+    print('out', out.size())
+    return out
+
+
 if __name__ == '__main__':
     G = create_simple_graph()
-    nx.draw(G, with_labels=True, font_weight='bold')
-    plt.show()
-    print("Done")
+    draw_weighted_graph(G)
+    out = compute_graph_convolution(G, K=1, normalization='sym')
+
+    #node_feat = torch.tensor([v for v in node_features.values()])
+    #node_features = torch.tensor([G.nodes[i]['feat'] for i in G.nodes()])
+    #print(node_feat.size())
+    #weights = torch.tensor([w for w in edge_weights.values()])
+    #print(weights.size())
+
+
+    weights = torch.tensor([G[u][v]['weight'] for u, v in G.edges()])
+    print('weights', weights.size())
+    
+
