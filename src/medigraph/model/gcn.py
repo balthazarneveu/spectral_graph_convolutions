f370aa730e66a778cd48db373a69a564072d0154
InesVATI <ines.vati@eleves.enpc.fr>
2023-12-09 12:22:52 +0100

    add BinaryGCN and mask train

--- src/medigraph/model/gcn.py
+++ src/medigraph/model/gcn.py
@@ -1,4 +1,9 @@
+from typing import Optional, Union, List
 import torch
+from scipy import sparse 
+import numpy as np
+import torch.nn.functional as F
+# from torch.nn.parameter import Parameter
 
 
 class BasicGCNDenseLayer(torch.nn.Module):
@@ -44,3 +49,69 @@ class GCN(torch.nn.Module):
         x = self.gcn2(x)
         logit = self.classifier(x)
         return logit.squeeze()
+
+
+class SimpleGraphConvolution(torch.nn.Module):
+    def __init__(self, in_features, out_features, bias: bool=True):
+        super().__init__()
+        print(f"in_feat {in_features} out_feat {out_features}")
+        self.out_features = out_features
+        self.fc = torch.nn.Linear(in_features, out_features, bias=bias)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / np.sqrt(self.out_features)
+        self.fc.weight.data.uniform_(-stdv, stdv)
+        if self.fc.bias:
+            self.fc.bias.data.uniform_(-stdv, stdv)
+    
+    def forward(self, x: torch.Tensor, adj: torch.Tensor):
+        support = self.fc(x)
+        output = torch.spmm(adj, support)
+
+        return output
+
+class SparseGCN(torch.nn.Module):
+    """Graph Convolutional Network based of http://arxiv.org/abs/1609.02907"""
+
+    def get_normalized_adjacency_matrix(adj: np.array, dtype : torch.dtype=torch.float32):
+        """ Apply the renormalization trick 
+        return a sparse matrix """
+
+        adj = adj + sparse.eye(adj.shape[0]) # warning : addition is valif if adj is a numpy.array but not if it a tensor
+        tilde_d = np.array(1 / adj.sum(1)).flatten()
+        tilde_d[np.isinf(tilde_d)] = 0.
+        tilde_d = sparse.diags(tilde_d)
+        adj = tilde_d.dot(adj)
+
+
+        return torch.tensor(adj, dtype=dtype).to_sparse()
+    
+    def __init__(self, nfeat: int, nhid: Union[int, List[int]], nclass: int, adjacency: np.array, proba_dropout: float = 0.5):
+        super().__init__()
+        assert type(adjacency) == np.ndarray, "For initialization, adjacency matrix must be an array"
+        self.adj = SparseGCN.get_normalized_adjacency_matrix(adjacency)
+        if isinstance(nhid, int):
+            nhid = [nhid]
+        self.gc_first = SimpleGraphConvolution(nfeat, nhid[0], bias=False)
+        self.gc_hid = torch.nn.ModuleList()
+        for i, hid in enumerate(nhid[1:]):
+            self.gc_hid.append(
+                SimpleGraphConvolution(nhid[i], hid, bias=False)
+            )
+        self.gc_last = SimpleGraphConvolution(nhid[-1], nclass, bias=False)
+
+        self.proba_dropout = proba_dropout
+
+    def forward(self, x: torch.Tensor):
+        x = F.relu(self.gc_first(x, self.adj))
+        x = F.dropout(x, self.proba_dropout, training=self.training)
+
+        for gc in self.gc_hid:
+            x = F.relu( gc(x, self.adj))
+            x = F.dropout(x, self.proba_dropout, training=self.training)
+
+        x = self.gc_last(x, self.adj) # (n, nclass)
+        # x = F.softmax(x, dim=1) # warning : usually for multi-class or when the loss is nnllloss
+        return x
+    
--- src/medigraph/train.py
+++ src/medigraph/train.py
@@ -1,12 +1,16 @@
 """Training loop
 """
 import torch
-try:
-    __IPYTHON__  # noqa: F821
-    from tqdm.notebook import tqdm
-except Exception as _e:  # noqa: F841
-    from tqdm import tqdm
-from medigraph.data.properties import INPUTS, LABELS
+import matplotlib.pyplot as plt
+from tqdm import tqdm 
+
+# try:
+#     __IPYTHON__  # noqa: F821
+#     from tqdm.notebook import tqdm
+# except Exception as _e:  # noqa: F841
+#     from tqdm import tqdm
+
+from medigraph.data.properties import INPUTS, LABELS, TRAIN_MASK, VAL_MASK, TEST_MASK
 from typing import Tuple, Optional
 
 
@@ -49,3 +53,116 @@ def training_loop(
         "training_accuracies": training_accuracies
     }
     return model, metrics
+
+# ---- METRICS AND TEST ----
+
+def compute_accuracy(output, labels):
+
+    pred_label = (output >= 0.5).long()
+    correct = pred_label.eq(labels).double() 
+    correct = correct.sum()
+    return correct / len(labels)
+
+def test(model, data, criterion = torch.nn.BCEWithLogitsLoss()):
+    
+    model.eval()
+    input_feat = data[INPUTS]
+    labels = data[LABELS]
+    test_mask = data[TEST_MASK]
+    print(f'=== Computing metrics on {len(test_mask)} test nodes ===')
+    output_feat = model(input_feat)
+    loss_test = criterion(output_feat[test_mask], labels[test_mask])
+    acc_test = compute_accuracy(output_feat[test_mask], labels[test_mask])
+    return loss_test, acc_test
+
+# ---- PLOTTING ----
+
+def plot_learning_curves(train_log, val_log, title="Training"):
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+
+    metrics = ["losses", "accuracies"]
+    for i in range(len(metrics)):
+        ax[i].plot(train_log[metrics[i]], label="train")
+        ax[i].plot(val_log[metrics[i]], label="val")
+        ax[i].legend()
+        ax[i].set_title(metrics[i].upper())
+        ax[i].set_xlabel("Epochs")
+
+    fig.suptitle(title)
+
+# --- TRAIN LOOP FOR BINARY CLASSIFIER ---
+
+
+# def get_training_dict(data, device: torch.device = device,
+#              nb_train: int = 600, 
+#              nb_val: int = 100):
+#     inp, labels, adj = data.get_training_data()
+
+#     inp = torch.tensor(inp, dtype=torch.float32).to(device)
+#     labels = torch.tensor(label, dtype=torch.float32).to(device)
+#     labels = torch.stack([1-labels, labels], dim=1)
+
+#     clean_inp = sanitize_data(inp)
+#     inp = whiten(clean_inp)
+
+#     # get random masks
+#     shuffle_nodes = np.random.permutation(range(inp.shape[0]))
+#     train_mask = shuffle_nodes[:nb_train]
+#     val_mask = shuffle_nodes[nb_train:nb_train+nb_val]
+#     test_mask = shuffle_nodes[nb_train+nb_val:]
+
+#     train_mask = torch.LongTensor(train_mask) #int64 tensor
+#     val_mask = torch.LongTensor(val_mask)
+#     test_mask = torch.LongTensor(test_mask)
+
+#     return { INPUTS : inp,
+#             LABELS : labels,
+#             TRAIN_MASK : train_mask,
+#             VAL_MASK : val_mask,
+#             TEST_MASK : test_mask
+#             }
+
+
+def train(model : torch.nn.Module, 
+          data: dict,
+          nEpochs: Optional[int] = 150,
+          criterion: torch.nn.Module = torch.nn.BCEWithLogitsLoss(),
+          optimizer_params: Optional[dict ]={"lr": 5.E-3,
+                                            "weight_decay": 5.E-4}
+    )-> Tuple[torch.nn.Module, dict, dict]:
+    
+    optim = torch.optim.Adam(model.parameters(), **optimizer_params)
+    train_log = {
+        "losses": [],
+        "accuracies": []
+    }
+    val_log = {
+        "losses": [],
+        "accuracies": []
+    }
+
+    input_feat = data[INPUTS]
+    labels = data[LABELS]
+    train_mask = data[TRAIN_MASK]
+    val_mask = data[VAL_MASK]
+
+    model.train()
+    for _ in tqdm(range(nEpochs)):
+        optim.zero_grad()
+
+        output_feat = model(input_feat)
+        loss_train = criterion(output_feat[train_mask], labels[train_mask])
+        loss_train.backward()
+        optim.step()
+
+        with torch.no_grad():
+            # model.eval()
+            acc_train = compute_accuracy(output_feat[train_mask], labels[train_mask])
+            loss_val = criterion(output_feat[val_mask], labels[val_mask])
+            acc_val = compute_accuracy(output_feat[val_mask], labels[val_mask])
+            train_log["losses"].append(loss_train.item())
+            train_log['accuracies'].append(acc_train.item())
+            val_log['losses'].append(loss_val.item())
+            val_log['accuracies'].append(acc_val.item())
+
+    return model, train_log, val_log
