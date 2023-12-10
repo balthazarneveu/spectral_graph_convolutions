a4ea0df2cfa94b5a246f42bc40bd699499177b5e
InesVATI <ines.vati@eleves.enpc.fr>
2023-12-10 12:56:23 +0100

    start umap

--- scripts/train_script.py
+++ scripts/train_script.py
@@ -1,51 +1,43 @@
-from medigraph.train import training_loop
-
-from medigraph.data.abide import AbideData
-from medigraph.model.gcn import GCN
-from medigraph.model.baseline import DenseNN
+from pathlib import Path
+from medigraph.data.properties import (
+    RFE_DIM_REDUCTION, RAW_INP, NORMALIZED_INPUTS
+)
+import argparse
+from medigraph.model.metrics import plot_metrics, analyze_metrics
+from medigraph.train_multiple_models import train_multiple_configurations as train
 import torch
-from medigraph.data.preprocess import sanitize_data, whiten
-from medigraph.data.properties import INPUTS, LABELS, ADJ
-from medigraph.model.metrics import plot_metrics
-import logging
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def prepare_training_data(device=DEVICE):
-    dat = AbideData()
-    data_dict = dat.get_training_data(dimension_reduction="rfe")
-    adj_np = data_dict[ADJ]
-    lab_np = data_dict[LABELS]
-    inp_np = data_dict[INPUTS]
-    logging.info(f"Adjacency matrix : {adj_np.shape} [VxV]")
-    logging.info(f"Labels {lab_np.shape} : [V]")
-    logging.info(f"Input feature vector {inp_np.shape} : [VxF]")
+def main():
+    models_list = ["Dense", "Dense-dr=0.1", "Dense-dr=0.2", "Dense-dr=0.3"]
+    models_list = ["GCN-dr=0.1",]
+    # models_list = ["GCN", "GCN-dr=0.3"]
+    models_list = ["Single-h=1", "Single-h=4", "Single-h=8", "Single-h=16", "Dense", "Single-h=128"]
+    # models_list+= ["GCN-dr=0.1",]
+    # models_list += ["Cheb-dr=0.3"]
 
-    adj = torch.tensor(adj_np, dtype=torch.float32).to(device)
-    inp = torch.tensor(inp_np, dtype=torch.float32).to(device)  # [V=871,  F6216]
-    lab = torch.tensor(lab_np, dtype=torch.float32).to(device)  # for binary classification
-    training_data = {
-        INPUTS: inp,
-        LABELS: lab
-    }
-    return training_data, adj
-
-
-def train(device=DEVICE):
-    training_data, adj = prepare_training_data(device=device)
-    metric_dict = {}
-    feat_dim = training_data[INPUTS].shape[1]
-    for model_name in ["Dense", "GCN"]:
-        if model_name == "GCN":
-            model = GCN(feat_dim, adj, hdim=64)
-        else:
-            model = DenseNN(feat_dim, hdim=64)
-        model.to(device)
-        model, metrics = training_loop(model, training_data, device=device, n_epochs=1000)
-        metric_dict[model_name] = metrics
+    parser = argparse.ArgumentParser(description="Train classification models on Abide dataset - compare performances")
+    parser.add_argument("-d", "--device", type=str,
+                        choices=["cpu", "cuda"], default=str(DEVICE), help="Training device")
+    parser.add_argument("-n", "--n-epochs", type=int, default=1000, help="Number of epochs")
+    parser.add_argument("-o", "--output-folder", type=str, default="results", help="Output results folder"),
+    parser.add_argument("-m", "--models-list", nargs="+", default=models_list, help="List of models to train")
+    parser.add_argument("-f", "--features-selection", nargs="+", default=[RFE_DIM_REDUCTION],
+                        choices=[RFE_DIM_REDUCTION, NORMALIZED_INPUTS, RAW_INP],
+                        help="List of input feature reduction methods")
+    args = parser.parse_args()
+    metric_dict = train(
+        models_list=args.models_list,
+        device=args.device,
+        n_epochs=args.n_epochs,
+        features_selection_list=args.features_selection,
+        output_folder=Path(args.output_folder)
+    )
+    analyze_metrics(metric_dict, plot_flag=True)
     plot_metrics(metric_dict)
 
 
 if __name__ == "__main__":
-    train()
+    main()
--- src/medigraph/train.py
+++ src/medigraph/train.py
@@ -11,47 +11,68 @@ from tqdm import tqdm
 #     from tqdm import tqdm
 
 from medigraph.data.properties import INPUTS, LABELS, TRAIN_MASK, VAL_MASK, TEST_MASK
+from medigraph.model.metrics import TRAIN, VALIDATION, TEST
 from typing import Tuple, Optional
 
 
 def training_loop(
     model: torch.nn.Module,
     train_data: dict, device,
-    valid_data: Optional[dict] = None,
+    noise_level: Optional[float] = None,
     optimizer_params={
         "lr": 1.E-4,
         "weight_decay": 0.1
     },
     n_epochs: Optional[int] = 1000
 ) -> Tuple[torch.nn.Module, dict]:
+
     criterion = torch.nn.BCEWithLogitsLoss()
     train_input = train_data[INPUTS]
     train_label = train_data[LABELS]
+
+    train_mask = train_data[TRAIN_MASK]
+    val_mask = train_data[VAL_MASK]
+    test_maks = train_data[TEST_MASK]
+
     model.to(device)
     optim = torch.optim.Adam(model.parameters(), **optimizer_params)
-    training_losses = []
-    training_accuracies = []
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, 'min')
+    # scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.999)
+    losses = {TRAIN: [], VALIDATION: [], TEST: []}
+    accuracies = {TRAIN: [], VALIDATION: [], TEST: []}
     for ep in tqdm(range(n_epochs)):
         model.train()
         optim.zero_grad()
-        logit = model(train_input)
-        loss = criterion(logit, train_label)
+        if noise_level is not None:
+            train_input_noisy = train_input + noise_level * torch.randn_like(train_input)
+        else:
+            train_input_noisy = train_input
+        logit = model(train_input_noisy)
+        loss = criterion(logit[train_mask], train_label[train_mask])
         loss.backward()
         optim.step()
+        model.eval()
         with torch.no_grad():
-            predicted_prob = torch.sigmoid(logit).squeeze()  # Apply sigmoid and remove extra dimensions if any
-            predicted = (predicted_prob >= 0.5).long()  # Convert probabilities to 0 or 1
-            correct = (predicted == train_label).sum().item()
-            total = train_label.shape[0]
-            accuracy = correct / total
-            training_accuracies.append(accuracy)
+            for mode, mask in zip([TRAIN, VALIDATION, TEST], [train_mask, val_mask, test_maks]):
+                if mask is None:
+                    continue
+                loss = criterion(logit[mask], train_label[mask])
+                predicted_prob = torch.sigmoid(logit[mask]).squeeze()  # Apply sigmoid and remove extra dimensions
+                predicted = (predicted_prob >= 0.5).long()  # Convert probabilities to 0 or 1
+                correct = (predicted == train_label[mask]).sum().item()
+                total = len(mask)
+                accuracy = correct / total
+                losses[mode].append(loss.detach().cpu())
+                accuracies[mode].append(accuracy)
         if ep % 100 == 0:
-            print(f"Epoch {ep} loss: {loss.item():10f} - accuracy: {accuracy:.2%}")
-        training_losses.append(loss.detach().cpu())
-    metrics = {
-        "training_losses": training_losses,
-        "training_accuracies": training_accuracies
-    }
+            print(
+                f"Epoch {ep}\nloss: train {losses[TRAIN][-1]:.5f} | validation {losses[VALIDATION][-1]:.5f}" +
+                f"\naccuracy: train {accuracies[TRAIN][-1]:.2%} | validation {accuracies[VALIDATION][-1]:.2%}")
+            print("Learning rate", optim.param_groups[0]['lr'])
+        # scheduler.step(losses[VALIDATION][-1])
+        # scheduler.step()
+        
+    metrics = {"loss": losses, "accuracy": accuracies}
     return model, metrics
 
 # ---- METRICS AND TEST ----
