e3fdbba58d47d4f2019f258deddbe38dc87d9ac0
Balthazar Neveu <balthazarneveu@gmail.com>
2023-12-10 03:02:05 +0100

    metrics on cross validation

--- src/medigraph/model/metrics.py
+++ src/medigraph/model/metrics.py
@@ -36,7 +36,7 @@ def plot_metrics(metric_dict: dict) -> None:
         }
         ```
     """
-    fig, axs = plt.subplots(1, 2, figsize=(10, 6))
+    fig, axs = plt.subplots(1, 3, figsize=(10, 6))
     colors = ["b", "g", "r", "y", "k"]
     # colors  = [""]
     for idx, (model_name, metric) in enumerate(metric_dict.items()):
@@ -46,7 +46,7 @@ def plot_metrics(metric_dict: dict) -> None:
         axs[0].plot(metric[LOSS][TEST], color+"-", linewidth=2, label=model_name + " TEST")
         axs[1].plot(metric[ACCURACY][TRAIN], color+"--", label=f"{model_name} TRAIN accuracy")
         axs[1].plot(metric[ACCURACY][VALIDATION], color+"-.", alpha=0.8, label=f"{model_name} VALIDATION accuracy")
-        axs[1].plot(metric[ACCURACY][TEST], color+"-", linewidth=2, label=f"{model_name} TEST accuracy")
+        axs[2].plot(metric[ACCURACY][TEST], color+"-", linewidth=2, label=f"{model_name} TEST accuracy")
     for ax in axs:
         ax.legend()
         ax.grid()
@@ -54,7 +54,9 @@ def plot_metrics(metric_dict: dict) -> None:
     axs[0].set_ylabel("Binary Cross Entropy Loss")
     axs[1].set_xlabel("Epochs")
     axs[1].set_ylabel("Accuracy")
+    axs[2].set_ylabel("Test accuracy")
     axs[0].set_title("Losses")
     axs[1].set_title("Accuracy")
+    axs[2].set_title("Test accuracy")
 
     plt.show()
