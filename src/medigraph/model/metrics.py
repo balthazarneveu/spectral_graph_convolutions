6bf402504598004df42b4ca91e928ecfbeba7171
Balthazar Neveu <balthazarneveu@gmail.com>
2023-12-10 03:38:32 +0100

    plot average test accuracy

--- src/medigraph/model/metrics.py
+++ src/medigraph/model/metrics.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 TRAIN = "train"
 VALIDATION = "validation"
 TEST = "test"
@@ -43,18 +43,29 @@ def plot_metrics(metric_dict: dict) -> None:
         color = colors[idx % len(colors)]
         for seed_idx, seed in enumerate(metric.keys()):
             current_metric = metric[seed]
-            print(seed)
-            axs[0].plot(current_metric[LOSS][TRAIN], color+"--", label=None if seed_idx >= 1 else model_name + " TRAIN")
-            axs[0].plot(current_metric[LOSS][VALIDATION], color+"-.", alpha=0.8,
-                        label=None if seed_idx >= 1 else model_name + " VALIDATION")
+            axs[0].plot(current_metric[LOSS][TRAIN], color+"--",
+                        label=None if seed_idx >= 1 else (model_name + " TRAIN"))
+            axs[0].plot(current_metric[LOSS][VALIDATION], color+"-.",
+                        alpha=0.8,
+                        label=None if seed_idx >= 1 else (model_name + " VALIDATION"))
             axs[0].plot(current_metric[LOSS][TEST], color+"-", linewidth=2,
-                        label=None if seed_idx >= 1 else model_name + " TEST")
+                        label=None if seed_idx >= 1 else (model_name + " TEST"))
             axs[1].plot(current_metric[ACCURACY][TRAIN], color+"--",
-                        label=None if seed_idx >= 1 else f"{model_name} TRAIN accuracy")
-            axs[1].plot(current_metric[ACCURACY][VALIDATION], color+"-.", alpha=0.8,
-                        label=None if seed_idx >= 1 else f"{model_name} VALIDATION accuracy")
-            axs[2].plot(current_metric[ACCURACY][TEST], color+"-", linewidth=2,
-                        label=None if seed_idx >= 1 else f"{model_name} TEST accuracy")
+                        label=None if seed_idx >= 1 else (f"{model_name} TRAIN accuracy"))
+            axs[1].plot(current_metric[ACCURACY][VALIDATION], color+"-.",
+                        alpha=0.8,
+                        label=None if seed_idx >= 1 else (f"{model_name} VALIDATION accuracy"))
+            axs[2].plot(current_metric[ACCURACY][TEST], color+"-",
+                        # linewidth=2,
+                        alpha=0.2,
+                        label=None if seed_idx >= 1 else (f"{model_name} TEST accuracy"))
+        acc_test = np.array([current_metric[ACCURACY][TEST] for seed in metric.keys()]).mean(axis=0)
+        axs[2].plot(
+            acc_test,
+            color+"-",
+            linewidth=3,
+            alpha=1.,
+            label=f"{model_name} average TEST accuracy")
     for ax in axs:
         ax.legend()
         ax.grid()
